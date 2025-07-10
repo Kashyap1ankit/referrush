62dd6dc8af312965a6c4dadb0cc3f2bbfb3f57e2
Ankit <kashyap25ankit@gmail.com>
2025-07-10 14:13:07 +0530

    hero section

--- src/components/hero-sections.tsx
+++ src/components/hero-sections.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from "react";
-import { FaPlay, FaPause, FaShopify } from "react-icons/fa6";
+import { FaPause, FaShopify } from "react-icons/fa6";
+import { FaPlay } from "react-icons/fa";
 import { Button } from "./ui/button";
 import { BsGraphUpArrow } from "react-icons/bs";
 import { SiCalendly } from "react-icons/si";
@@ -27,8 +28,8 @@ export default function HeroSection() {
         filter: "blur(0px)",
       }}
     >
-      <motion.p
-        className="text-7xl text-white font-primary font-bold text-center text-white"
+      <motion.div
+        className="flex items-center gap-2 bg-btn-primary  p-2 rounded-md border border-dashed border-btn-secondary w-fit mx-auto mb-6"
         initial={{
           filter: "blur(10px)",
           y: 30,
@@ -42,8 +43,30 @@ export default function HeroSection() {
           ease: "easeInOut",
         }}
       >
+        <p className="text-md font-secondary font-semibold text-white">
+          Backed by{" "}
+        </p>
+        <img src="/wtf.png" className=" w-10" />
+      </motion.div>
+
+      <motion.p
+        className="text-7xl text-white font-primary font-bold text-center text-white"
+        initial={{
+          filter: "blur(10px)",
+          y: 30,
+        }}
+        animate={{
+          filter: "blur(0px)",
+          y: 0,
+        }}
+        transition={{
+          duration: 1,
+
+          ease: "easeInOut",
+        }}
+      >
         Boost Sales with Automated, Omni Channel{" "}
-        <span className="bg-linear-to-l  from-green-600 to-green-500 text-transparent bg-clip-text">
+        <span className="bg-linear-to-l  from-btn-secondary to-green-500 text-transparent bg-clip-text">
           Referrals
         </span>
       </motion.p>
@@ -91,7 +114,7 @@ export default function HeroSection() {
         }}
       >
         <Button
-          className="rounded-full bg-linear-to-t from-green-900 to-green-600  text-white font-primary text-lg font-semibold group cursor-pointer px-12 has-[>svg]:px-8"
+          className="rounded-full bg-linear-to-t from-btn-primary to-btn-secondary  text-white font-primary text-lg font-semibold group cursor-pointer px-12 has-[>svg]:px-8"
           variant={"default"}
           size={"lg"}
         >
@@ -109,7 +132,7 @@ export default function HeroSection() {
       </motion.div>
 
       <div className="absolute top-62 w-full">
-        <div className="flex justify-between px-24">
+        <div className="flex justify-between px-16">
           <motion.div
             initial={{
               y: 0,
@@ -122,7 +145,7 @@ export default function HeroSection() {
               repeat: Infinity,
             }}
           >
-            <LuBadgeDollarSign className="rounded-lg bg-radial-[at_25%_25%] from-green-600 to-green-800  text-white size-16 p-4  -rotate-z-24" />
+            <LuBadgeDollarSign className="rounded-lg bg-radial-[at_25%_25%] from-btn-secondary to-green-800  text-white size-16 p-4  -rotate-z-24" />
           </motion.div>
           <motion.div
             initial={{
@@ -136,41 +159,36 @@ export default function HeroSection() {
               repeat: Infinity,
             }}
           >
-            <BsGraphUpArrow className="rounded-lg bg-radial-[at_25%_25%] from-green-600 to-green-800  text-white size-16 p-4 rotate-z-24" />
+            <BsGraphUpArrow className="rounded-lg bg-radial-[at_25%_25%] from-btn-secondary to-green-800  text-white size-16 p-4 rotate-z-24" />
           </motion.div>
         </div>
       </div>
 
-      <div className="mt-12 relative w-full">
+      <div className="mt-24 relative w-full h-[40rem] [mask-image:linear-gradient(to_top,transparent,black_60%)] ">
         <video
           loop
           muted
           ref={vidRef}
-          className="w-full h-auto object-cover rounded-lg"
+          className={`w-full h-auto object-cover rounded-2xl border border-zinc-800 ${
+            !playVideo ? "blur-xs" : ""
+          }`}
         >
           <source src="demo.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
 
-        <Button
+        <div
           onClick={() => setPlayVideo(!playVideo)}
-          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 focus:outline-none"
+          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 focus:outline-none bg-btn-primary  size-20 flex items-center justify-center  rounded-full p-6  transition-all duration-300 hover:scale-110 cursor-pointer"
           aria-label={playVideo ? "Pause video" : "Play video"}
         >
           {playVideo ? (
             <FaPause className="size-20 text-white drop-shadow-lg" />
           ) : (
-            <FaPlay className="size-28 text-white bg-black/50 rounded-full p-6 drop-shadow-lg" />
+            <FaPlay className="size-20 text-white drop-shadow-lg" />
           )}
-        </Button>
+        </div>
       </div>
-
-      {/* <div className="relative z-50 mt-24">
-        <Safari
-          url="demo.mp4"
-          className="size-full shadow-2xl shadow-primary/30 dark:shadow-primary/20"
-        />
-      </div> */}
     </motion.div>
   );
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,16 +1,32 @@
 import { ArrowRight } from "lucide-react";
 import NavigationMenuDemo from "./nav-items";
 import { Button } from "./ui/button";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
+  const [isScrolled, setIsScrolled] = useState(false);
+  function handleScrollBehaviour() {
+    setIsScrolled(window.scrollY > 10);
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScrollBehaviour);
+
+    return () => window.removeEventListener("scroll", handleScrollBehaviour);
+  }, []);
+
   return (
-    <div className="fixed top-4 left-1/2 -translate-x-1/2 flex justify-between items-center border border-zinc-800 shadow-md bg-black/30  p-2 rounded-lg w-full md:w-1/2">
+    <div
+      className={`fixed top-4 left-1/2 -translate-x-1/2 flex justify-between items-center rounded-lg w-full md:w-1/2 p-2  z-999 border border-zinc-800 transition-all duration-300 ${
+        isScrolled ? " shadow-md bg-black/90 backdrop-md" : ""
+      }  `}
+    >
       <div className="flex items-center gap-8">
         <img src="/logo.png" className="w-12 " />
         <NavigationMenuDemo />
       </div>
       <Button
-        className="rounded-full bg-linear-to-t from-green-900 to-green-600  text-white font-primary text-lg font-semibold group cursor-pointer px-12 has-[>svg]:px-8"
+        className="rounded-full bg-linear-to-t from-btn-primary to-btn-secondary  text-white font-primary text-lg font-semibold group cursor-pointer px-12 has-[>svg]:px-8"
         variant={"default"}
         size={"lg"}
       >
